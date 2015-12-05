3173bcdc6665980bc44c1f11634a4adc61e369ba
faheem <faheem@faheempatel.com>
2015-12-05 17:59:46 +0000

    Minor stuff

--- intro/sketch.js
+++ intro/sketch.js
@@ -11,27 +11,27 @@ r.rect(0, 0, 1920, 1200)
 for (var i = -3; i < 15; i++) {
   for (var y = 0; y < 15; y++) {
     r.triangle(0 + (i * 120), 0 + (y * 120), 30 + (i * 120), 85 + (y * 120), 60 + (i * 120), 0 + (y * 120))
-    .fill(255, 255, 255)
-    .strokeWidth(5)
-    .rotate(335);
+     .fill(255, 255, 255)
+     .strokeWidth(5)
+     .rotate(335);
 
     r.circle(10 + (i * 120), 20 + (y * 120), 20)
-      .fill(255, 255, 255)
-      .stroke(0, 0, 0)
-      .strokeWidth(5)
-      .rotate(335);
+     .fill(255, 255, 255)
+     .stroke(0, 0, 0)
+     .strokeWidth(5)
+     .rotate(335);
 
     r.circle(50 + (i * 120), 20 + (y * 120), 20)
-      .fill(255, 255, 255)
-      .stroke(0, 0, 0)
-      .strokeWidth(5)
-      .rotate(335);
+     .fill(255, 255, 255)
+     .stroke(0, 0, 0)
+     .strokeWidth(5)
+     .rotate(335);
 
     r.circle(30 + (i * 120), 0 + (y * 120), 30)
-      .fill(255, 255, 255)
-      .stroke(0, 0, 0)
-      .strokeWidth(5)
-      .rotate(335);
+     .fill(255, 255, 255)
+     .stroke(0, 0, 0)
+     .strokeWidth(5)
+     .rotate(335);
   }
 }
 
